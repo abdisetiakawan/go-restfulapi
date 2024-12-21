c976417aab8d5f453199e0ffb19ed826cdee41f3
abdisetiakawan <bedikadiryt@gmail.com>
2024-12-21 13:46:26 +0700

    `change Category ID type from uint to int for consistency`

--- internal/model/category.go
+++ internal/model/category.go
@@ -1,6 +1,6 @@
 package model
 
 type Category struct {
-    ID   uint   `gorm:"primaryKey;autoIncrement"`
+    ID   int   `gorm:"primaryKey;autoIncrement"`
     Name string `gorm:"type:varchar(200);not null"`
 }
--- internal/model/web/category_response.go
+++ internal/model/web/category_response.go
@@ -1,6 +1,6 @@
 package web
 
 type CategoryResponse struct {
-	ID   uint   `json:"id"`
+	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
