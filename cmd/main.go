e5bccb451dde3c40b5bd2950bd59841c2a63ddb3
abdisetiakawan <bedikadiryt@gmail.com>
2024-12-21 13:45:35 +0700

    `refactor main function to initialize server and category management routes`

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/abdisetiakawan/go-restfulapi/internal/config"
-	"github.com/abdisetiakawan/go-restfulapi/internal/model"
+	"github.com/abdisetiakawan/go-restfulapi/internal/controller"
+	"github.com/abdisetiakawan/go-restfulapi/internal/repository"
+	"github.com/abdisetiakawan/go-restfulapi/internal/service"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
     // Initialize database
-    db, err := config.NewDatabaseConnection()
-    if err != nil {
-        log.Fatal("Failed to connect to database:", err)
-    }
+    db := config.NewDB()
+    // Initialize validation
+    categoryRepository := repository.NewCategoryRepository()
+    categoryService := service.NewCategoryService(categoryRepository, db)
+    categoryController := controller.NewCategoryController(categoryService)
 
-    // Run migration
-    err = db.AutoMigrate(&model.Category{})
+    router := httprouter.New()
+    router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        w.Write([]byte("Hello World!"))
+    })
+    router.POST("/api/categories", categoryController.Create)
+    router.GET("/api/categories/:id", categoryController.FindById)
+    router.GET("/api/categories", categoryController.FindAll)
+    router.PUT("/api/categories/:id", categoryController.Update)
+    router.DELETE("/api/categories/:id", categoryController.Delete)
+
+
+    // Start server
+    server := http.Server{
+        Addr:    "localhost:3000",
+        Handler: router,
+    }
+    log.Println("Starting server on localhost:3000")
+    err := server.ListenAndServe()
     if err != nil {
-        log.Fatal("Failed to migrate database:", err)
+        log.Fatal(err)
     }
 }
